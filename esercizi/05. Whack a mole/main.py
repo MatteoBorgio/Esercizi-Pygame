c05c8f6710f7ddcbee7095b522f66283332e2bd1
matteoborgio <matteo6.borgio@gmail.com>
2026-04-08 12:44:23 +0200

    aggiunta struttura es.5 whack a mole

---
+++ esercizi/05. Whack a mole/main.py
@@ -0,0 +1,8 @@
+# 1. Perchè rende più facile l'iterazione e la gestione del click del mouse.
+# 2. Per come è fatta la funzione hole_center() funzionerebbe ugualmente.
+# 3. Perchè vogliamo controllare un singolo click e non il click continuo
+
+from controller.game_controller import GameController
+
+if __name__ == "__main__":
+    GameController().run()
